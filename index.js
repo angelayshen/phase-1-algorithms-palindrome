e71d388b86872bd4294f8949cb82c3eca6da436d
Angela Shen <angelayshen@gmail.com>
2023-05-16 21:44:56 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+function reverseWord(word) {
+  return word.split("").reverse().join("")
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = reverseWord(word)
+  return word === reversedWord
 }
 
 /* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
+The reverseWord function reverses a word by combining three methods:
+1. split('') method to convert the string into an array of individual characters.
+2. reverse() method to reverse the order of the elements in the array.
+3. join('') method to join the elements of the array back into a single string.
 */
 
 // You can run `node index.js` to view these console logs
